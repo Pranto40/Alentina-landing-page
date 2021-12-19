ea8ae79dd2927bc52bf426552dd4503a8ab125a0
pranto <polasahammedpranto@gmail.com>
2021-12-19 22:35:44 +0600

    initial commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+"use strict"
 
 let menu = document.querySelector('#menu_btn');
 let navbar = document.querySelector('.navbar');
@@ -11,3 +12,26 @@ window.onscroll = () =>{
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
 };
+
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header .navbar ul li a');
+
+window.onscroll = () =>{
+
+  section.forEach(sec =>{
+
+    let top = window.scrollY;
+    let height = sec.offsetHeight;
+    let offset = sec.offsetTop - 150;
+    let id = sec.getAttribute('id');
+
+    if(top >= offset && top < offset + height){
+      navLinks.forEach(links =>{
+        links.classList.remove('active');
+        document.querySelector('header .navbar ul li a[href*='+id+']').classList.add('active');
+      });
+    };
+
+  });
+
+}
